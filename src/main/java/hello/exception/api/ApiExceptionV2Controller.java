5038bf7f81411832c99f4e79e3c8b09d9c15ce76
1997kg <dojug3@naver.com>
2025-01-08 04:44:32 +0900

    @RestControllerAdvice(basePackages = "hello.exception.api")

--- src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -17,28 +17,29 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 public class ApiExceptionV2Controller {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandler(IllegalArgumentException e){
-        log.error("[exceptionHandler] ex", e);
-        return  new ErrorResult("BAD", e.getMessage());
-    }
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    @ExceptionHandler(IllegalArgumentException.class)
+//    public ErrorResult illegalExHandler(IllegalArgumentException e){
+//        log.error("[exceptionHandler] ex", e);
+//        return  new ErrorResult("BAD", e.getMessage());
+//    }
+//
+//
+//    @ExceptionHandler
+//    public ResponseEntity<ErrorResult> userExHandler(UserException e){
+//        log.error("[exceptionHandler] ex", e);
+//        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+//        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    @ExceptionHandler
+//    public ErrorResult exHandler(Exception e){
+//        log.error("[exceptionHandler] ex", e);
+//        return  new ErrorResult("EX", "내부 오류");
+//
+//    }
 
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> userExHandler(UserException e){
-        log.error("[exceptionHandler] ex", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult exHandler(Exception e){
-        log.error("[exceptionHandler] ex", e);
-        return  new ErrorResult("EX", "내부 오류");
-
-    }
     @GetMapping("api2/members/{id}")
     public MemberDto MemberDto (@PathVariable("id") String id){
 
